e1739f8feaf4e70b47bf0f5d3159119f4ba95933
Adar Budomski <adar688@gmail.com>
2025-01-09 14:00:43 +0200

    add variables to javaFX elements in the controller

--- DesignPatternsFinalProject/src/GUI/mainPageController.java
+++ DesignPatternsFinalProject/src/GUI/mainPageController.java
@@ -1,12 +1,66 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class mainPageController extends Application{
+	
+	@FXML
+	private TextField newAddressTxtBox;
+	
+	@FXML
+	private Button AddToListBtn;
+	
+	@FXML
+	private TableView<T> AddressToCheckTable;
+	
+	@FXML 
+	private TableColumn<T,T> AddressClmCheckTable;
+	
+	@FXML 
+	private TableColumn<T,T> EditClmCheckTable;
+	
+	@FXML 
+	private TableColumn<T,T> DeleteClmCheckTable;
+	
+	
+	@FXML
+	private Button clearTableBtn;
+	
+	@FXML
+	private TextArea logTxtArea;
+	
+	@FXML
+	private Button scanBtn;
+	
+	@FXML
+	private TableView<T> ResultsTable;
+	
+	@FXML 
+	private TableColumn<T,T> AddressClmReportTable;
+	
+	@FXML 
+	private TableColumn<T,T> NumOfAbusesClmReportTable;
+	
+	@FXML 
+	private TableColumn<T,T> LinkClimReportTable;
+	
+	@FXML
+	Button uploadFileBtn;
+	
+	@FXML
+	private Button saveResultsBtn;
+	
+	
 
 	public void start(Stage primaryStage) throws Exception {
 
