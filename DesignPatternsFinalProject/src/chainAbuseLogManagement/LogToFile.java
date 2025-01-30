b69604d292bc845a27ee3035b93c607e390b6356
Shwayke <pinskyshay@gmail.com>
2025-01-30 16:32:40 +0200

    made log to file

--- DesignPatternsFinalProject/src/chainAbuseLogManagement/LogToFile.java
+++ DesignPatternsFinalProject/src/chainAbuseLogManagement/LogToFile.java
@@ -1,10 +1,25 @@
 package chainAbuseLogManagement;
 
-public class LogToFile extends LogObserver{
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	@Override
-	public void update(String log) {
-		System.out.println("Log to file" + log);
-	}
-
+public class LogToFile extends LogObserver {
+    private String logFilePath;
+    
+    public LogToFile(String filePath) {
+        this.logFilePath = filePath;
+    }
+    
+    @Override
+    public void update(String log) {
+        try (FileWriter fw = new FileWriter(logFilePath, true);
+             PrintWriter writer = new PrintWriter(fw)) {
+            
+            writer.println(log);
+            
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
 }
