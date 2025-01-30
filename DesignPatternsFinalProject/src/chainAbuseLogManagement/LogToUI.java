c02e542bf27120df98a8062ed716eb316ffcd173
Shwayke <pinskyshay@gmail.com>
2025-01-30 17:02:07 +0200

    made log to ui class

--- DesignPatternsFinalProject/src/chainAbuseLogManagement/LogToUI.java
+++ DesignPatternsFinalProject/src/chainAbuseLogManagement/LogToUI.java
@@ -1,11 +1,19 @@
 package chainAbuseLogManagement;
+import javafx.scene.control.TextArea;
+import javafx.application.Platform;
 
-public class LogToUI extends LogObserver{
-
-	@Override
-	public void update(String log) {
-		System.out.println("Log To UI" + log);
-		
-	}
-
+public class LogToUI extends LogObserver {
+    private TextArea logArea;
+    
+    public LogToUI(TextArea logArea) {
+        this.logArea = logArea;
+    }
+    
+    @Override
+    public void update(String log) {
+        // Use Platform.runLater since we're updating UI from potentially different thread
+        Platform.runLater(() -> {
+            logArea.appendText(log + "\n");
+        });
+    }
 }
