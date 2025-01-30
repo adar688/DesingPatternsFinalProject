1eeb665333a6c3c811b21135e0fe735e600c3e94
Shwayke <pinskyshay@gmail.com>
2025-01-30 16:33:11 +0200

    updated Report construction

--- DesignPatternsFinalProject/src/chainAbuseAPIConn/Report.java
+++ DesignPatternsFinalProject/src/chainAbuseAPIConn/Report.java
@@ -6,21 +6,17 @@ import java.net.http.HttpResponse;
 
 public class Report {
 
+	// TODO: remove Link if still useless when were done
 	private String link;
 	private String address;
 	private int abuseCount;
 	
-	public Report(String url, String address,HttpResponse<String> report) {
+	public Report(String address,HttpResponse<String> report) {
 		this.address = address;
-		link = url + address;
+		link = "chainabuse.com/address/" + address; // TODO: add configuration
 		calcCount(report.body());
 	}
 	
-	public Report(String address) {	// TODO: add configuration
-		this.address = address;
-		link = "chainabuse.com/report/" + address;
-		abuseCount = 0;
-	}
 	/***
 	 * Gets a response body and returns the amount of reports in it (properties in the JSON)
 	 * @param body
@@ -55,29 +51,29 @@ public class Report {
 		return abuseCount;
 	}
 	
-	public static void main(String[] args) {
-		
-		HttpResponse<String> response = null;
-		 try {
-	            HttpRequest request = new ChainAbuseRequestBuilder("https://api.chainabuse.com/v0")
-	                .endpoint("/reports")
-	                .basicAuth("Y2FfV0RnelF6WkxZbEZhYkRaRlRHUlVaVnBWVVZZM1JYWlhMaTlKVWxnd2FuRXZkVVJ2ZG1vM09VOXVkMFZDWjJjOVBROmNhX1dEZ3pRelpMWWxGYWJEWkZUR1JVWlZwVlVWWTNSWFpYTGk5SlVsZ3dhbkV2ZFVSdmRtbzNPVTl1ZDBWQ1oyYzlQUQ==")
-	                .address("bc1qpzf5a7ucnfkwhgkxlyaywsyj6ennaspkcaxcpq")
-	                .build();
-	                
-	            response = HttpClient.newHttpClient()
-	                .send(request, HttpResponse.BodyHandlers.ofString());
-	            System.out.println("In the new builder");
-	            //System.out.println(request.toString());  
-	            System.out.println(response.body());
-	            
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-		
-		Report r = new Report("chainabuse.com/report/","bc1qpzf5a7ucnfkwhgkxlyaywsyj6ennaspkcaxcpq",response);
-		System.out.println(r.getAbuseCount());
-		System.out.println(r.getAddress());
-		System.out.println(r.getLink());
-	}
+//	public static void main(String[] args) {
+//		
+//		HttpResponse<String> response = null;
+//		 try {
+//	            HttpRequest request = new ChainAbuseRequestBuilder("https://api.chainabuse.com/v0")
+//	                .endpoint("/reports")
+//	                .basicAuth("Y2FfV0RnelF6WkxZbEZhYkRaRlRHUlVaVnBWVVZZM1JYWlhMaTlKVWxnd2FuRXZkVVJ2ZG1vM09VOXVkMFZDWjJjOVBROmNhX1dEZ3pRelpMWWxGYWJEWkZUR1JVWlZwVlVWWTNSWFpYTGk5SlVsZ3dhbkV2ZFVSdmRtbzNPVTl1ZDBWQ1oyYzlQUQ==")
+//	                .address("bc1qpzf5a7ucnfkwhgkxlyaywsyj6ennaspkcaxcpq")
+//	                .build();
+//	                
+//	            response = HttpClient.newHttpClient()
+//	                .send(request, HttpResponse.BodyHandlers.ofString());
+//	            System.out.println("In the new builder");
+//	            //System.out.println(request.toString());  
+//	            System.out.println(response.body());
+//	            
+//	        } catch (Exception e) {
+//	            e.printStackTrace();
+//	        }
+//		
+//		Report r = new Report("chainabuse.com/report/","bc1qpzf5a7ucnfkwhgkxlyaywsyj6ennaspkcaxcpq",response);
+//		System.out.println(r.getAbuseCount());
+//		System.out.println(r.getAddress());
+//		System.out.println(r.getLink());
+//	}
 }
